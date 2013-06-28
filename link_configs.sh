bbcb8e8152b1691c6fcdb0ee4a672d964acbb129
Patrick Reynolds <cpreynolds@gmail.com>
2013-06-28 00:19:14 -0400

    Adding a script to symlink the configs into the proper places.

---
+++ link_configs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir /Users/cpatrick/.todo
+ln -s "/Users/cpatrick/Projects/Misc/configuration/dot-todo-config" \
+      "/Users/cpatrick/.todo/config"
+
+ln -s "/Users/cpatrick/Projects/Misc/dot-profile" \
+      "/Users/cpatrick/.profile"
+
+ln -s "/Users/cpatrick/Projects/Misc/dot-gitconfig" \
+      "/Users/cpatrick/.gitconfig"
+
+ln -s "/Users/cpatrick/Projects/Misc/configuration/dot-ssh-config" \
+      "/Users/cpatrick/.ssh/config"
+
+ln -s "/Users/cpatrick/Projects/Misc/configuration/Preferences.sublime-settings" \
+      "/Users/cpatrick/Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings"
